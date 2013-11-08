4e799f8076a9a2fb348f71db86ef257f2a1632f8
root <root@node3.ibercivis.es>
2013-11-08 12:22:11 +0100

    Modify to give the correct answer

--- check_mounts.sh
+++ check_mounts.sh
@@ -17,13 +17,12 @@
 #	- Added test to touch a file in the mount, will only be checked if timeout util exists
 #	- Fixed EOL prints
 #=============================================================================================
-if [ -f  `basename $0`/utils.sh ];then
-        . `basename $0`/utils.sh
-else
-        STATE_OK=0
-        STATE_WARNING=1
-        STATE_CRITICAL=2
-fi
+
+# Exit codes
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+
 TMP_MOUNTS_FILE=/tmp/mounts1
 TMP_FSTAB_FILE=/tmp/fstab1
 CLUSTER_CONF_FILE=/etc/cluster/cluster.conf
@@ -52,6 +51,7 @@ if [ -f "$CLUSTER_CONF_FILE" ];then
                 fi
 		timeout -s9  10s touch $MP/tmpfile
 		if [ $? -ne 0 ];then
+
 		    MSG="$MSG\nFailure to touch a file under $MP/tmpfile :( \n"
 		    RC=$STATE_CRITICAL
 		fi
@@ -62,15 +62,16 @@ sort -u $TMP_MOUNTS_FILE >/tmp/mounts
 rm $TMP_MOUNTS_FILE
 TMP_MOUNTS_FILE=/tmp/mounts
 if diff -u $TMP_MOUNTS_FILE $TMP_FSTAB_FILE >/tmp/mounts_fstab.diff;then
-        echo -en "All mount points are properly mounted :)\n"
+        echo -en "MOUNTS OK: All mount points are properly mounted \n"
         if [ -n "$EXCLUDED" ];then
                 echo -en "Excluded (defined in $CLUSTER_CONF_FILE):$EXCLUDED\n"
         fi
-	if [ -z "$RC" ];then
+
+	#if [ -z "$RC" ];then
             RC=$STATE_OK
-	fi
+	#fi
 else
-        echo -en "/etc/fstab and /proc/mounts are not identical :(\n\n"
+        echo -en "MOUNT CRITICAL: /etc/fstab and /proc/mounts are not identical \n\n"
         MOUNTED_NO_TAB=`grep  "^-" /tmp/mounts_fstab.diff|sed 's/--- .*//g'`
         TAB_NOT_MOUNTED=`grep  "^+" /tmp/mounts_fstab.diff|sed 's/+++ .*//g'`
 	if [ -n "$MOUNTED_NO_TAB" ];then
@@ -78,9 +79,9 @@ else
 	<b>Warning: the following are mounted but do not have a matching /etc/fstab entry:</b>$MOUNTED_NO_TAB
 
 EOL`
-		if [ -z "$RC" ];then
+		#if [ -z "$RC" ];then
 		    RC=$STATE_WARNING
-		fi
+		#fi
 	fi
 	if [ -n "$TAB_NOT_MOUNTED" ];then
 	TAB_NOT_MOUNTED_MSG=`cat <<EOL
